248d76a86b488126da880c2fb034fb9586edd68d
Vaibhav Jain <vaibhavjain30699@gmail.com>
2022-02-07 21:18:11 +0530

    Added 2048 Logic

--- 2048-game.py
+++ 2048-game.py
@@ -1,6 +1,8 @@
 # Name: Vaibhav Jain
 # 2048 Game
 
+import random
+
 #Prints the Grid
 def printGrid(grid):
     print ("\n")
@@ -12,6 +14,70 @@ def printGrid(grid):
         print (res)
         print ("\n")
     return 0
+
+
+# Rotates the grid in clockwise direction
+def rotate(grid):
+    return list(map(list, zip(*grid[::-1])))
+
+
+# This function contains all the logic of the game
+def move(grid, dir):
+    for i in range(dir): grid = rotate(grid)
+    for i in range(len(grid)):
+        temp = []
+        for j in grid[i]:
+            if j != '.':
+                temp.append(j)
+        temp += ['.'] * grid[i].count('.') 
+        for j in range(len(temp) - 1):
+            if temp[j] == temp[j + 1] and temp[j] != '.' and temp[j + 1] != '.':
+                temp[j] = str(2 * int(temp[j]))
+                move.score += int(temp[j])
+                temp[j + 1] = '.'
+        grid[i] = []
+        for j in temp:
+            if j != '.':
+                grid[i].append(j)
+        grid[i] += ['.'] * temp.count('.')
+    for i in range(4 - dir): grid = rotate(grid)
+    return grid
+
+
+# This adds a random number to the grid
+def addNumber(grid):
+    num = random.randint(1, 2) * 2
+    x = random.randint(0, 3)
+    y = random.randint(0, 3)
+    lost = checkForMove(grid)
+    lost1 = False
+    if grid[x][y] != '.':
+        x, y, lost1 = findEmptySlot(grid)
+    if not lost1: grid[x][y] = str(num)
+    return (grid, lost and lost1)
+
+
+def checkForMove(grid):
+    for i in range(0,4):
+        for j in range(0,3):
+            if(grid[i][j]==grid[i][j+1]):
+                return False
+
+    for i in range(0,3):
+        for j in range(0,4):
+            if(grid[i][j]==grid[i+1][j]):
+                return False
+
+    return True
+
+
+# This checks for an empty slot where we can add a number
+def findEmptySlot(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == '.':
+                return (i, j, False)
+    return (-1, -1, True)
 
 
 def startGame():
@@ -29,6 +95,25 @@ def startGame():
 
     printGrid(grid)
     loseStatus = 0
+    move.score = 0
+    while True:
+        tmp = input("\nTo continue, Press 1 for left, 2 for bottom, 3 for right, 4 for top or\nPress X to end the game.\n")
+        if tmp in ["1", "2", "3", "4", "X", "x"]:
+            dir = direction[tmp.upper()]
+            if dir == 4:
+                print ("\nFinal score: " + str(move.score))
+                break
+            else:
+                grid = move(grid, dir)
+                grid, loseStatus = addNumber(grid)
+                printGrid(grid)
+                if loseStatus:
+                    print ("\nGame Over")
+                    print ("Final score: " + str(move.score))
+                    break
+                print ("\nCurrent score: " + str(move.score))
+        else:
+            print ("\nInvalid direction, please provide valid movement direction (1, 2, 3, 4).")
     return 0        
 
 # Starting the game
